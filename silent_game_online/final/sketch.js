98c234df3de40094b264cb299c047ab7bbfe7b19
chilicmit <114321161+chilicmit@users.noreply.github.com>
2022-09-27 10:05:58 -0400

    Update sketch.js

--- silent_game_online/final/sketch.js
+++ silent_game_online/final/sketch.js
@@ -252,7 +252,7 @@ function after_loading(){
   //text("or you can build on wherever you want ", 10, 620);
  // text("The game will start in 5 seconds ", 10, 660);
   image(img, width-600, height-370,600,300);
-  sleep(350);
+  sleep(100);
   
   
 }
